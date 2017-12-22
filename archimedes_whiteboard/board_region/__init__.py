303d99df6dd6d5124335a933ab0cfcb15ef1b277
Whillikers <aidanswope@gmail.com>
2017-12-22 02:38:37 -1000

    Cleaned up package structure and moved tests to top level

--- archimedes_whiteboard/board_region/__init__.py
+++ archimedes_whiteboard/board_region/__init__.py
@@ -3,6 +3,8 @@ Tools for detecting, extracting, and normalizing the whiteboard region from
 posted ArUco markers.
 """
 
+from archimedes_whiteboard.board_region.internals import (get_all_markers)
+
 
 def get_whiteboard_region_normal(whiteboard_image):
     """
@@ -11,3 +13,8 @@ def get_whiteboard_region_normal(whiteboard_image):
     perspective transform.
     """
     pass
+
+
+__all__ = [
+    'get_all_markers'
+]
--- archimedes_whiteboard/test/draw_markers.py
+++ archimedes_whiteboard/test/draw_markers.py
@@ -3,10 +3,10 @@ Test board region extraction functions.
 """
 
 import cv2
-from .. import board_region
+from archimedes_whiteboard import board_region
 
 
-img = cv2.imread('../../sample_images/1-angled.jpg')
+img = cv2.imread('../sample_images/1-angled.jpg')
 markers = board_region.get_all_markers(img)
 corners, ids, rejected = markers
 img_with_markers = cv2.aruco.drawDetectedMarkers(img, corners, ids)
