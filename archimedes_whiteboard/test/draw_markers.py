baca000e73f1abd8d7cb49eae5798bb8ad51bd92
Whillikers <aidanswope@gmail.com>
2017-12-23 21:34:15 -1000

    Made marker drawing test description slightly more precise

--- archimedes_whiteboard/test/draw_markers.py
+++ archimedes_whiteboard/test/draw_markers.py
@@ -1,5 +1,5 @@
 """
-Test board marker detection.
+Test board region extraction functions.
 """
 
 import cv2
@@ -14,6 +14,4 @@ img_with_markers = cv2.aruco.drawDetectedMarkers(img, corners, ids)
 
 resized = cv2.resize(img_with_markers, (1600, 1600))
 cv2.imshow("markers", resized)
-while True:
-    if cv2.waitKey(1) == ord('q'):
-        break
+cv2.waitKey(0) # Window closes on key press
