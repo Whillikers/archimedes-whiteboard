e4a32fbc0c49a5af5fe1618a1d23799342bd9fcd
Whillikers <aidanswope@gmail.com>
2018-01-27 23:17:06 -0800

    Changed command config file name

--- archimedes_whiteboard/test/commands.py
+++ archimedes_whiteboard/test/commands.py
@@ -10,7 +10,7 @@ from archimedes_whiteboard.board_region import get_whiteboard_region_normal
 img = cv2.imread('../sample_images/2-thicklines-angled.jpg')
 normalized = get_whiteboard_region_normal(img)
 
-with open('../config.yml') as config:
+with open('../tasks.yml') as config:
     commands = list(yaml.load_all(config))
     for task in commands:
         task.act_on_frame(normalized)
